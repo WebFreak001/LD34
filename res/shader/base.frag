5f27c59ee664f5c44722dd558ca2971286c80a69
WebFreak001 <janju007@web.de>
2015-12-12 22:57:35 +0100

    Added health & game over & menu

--- res/shader/base.frag
+++ res/shader/base.frag
@@ -1,9 +1,10 @@
 #version 330
 uniform sampler2D tex;
 uniform vec3 color;
+uniform float opacity;
 in vec2 texCoord;
 layout(location = 0) out vec4 out_frag_color;
 void main()
 {
-	out_frag_color = texture(tex, texCoord) * vec4(color, 1);
+	out_frag_color = texture(tex, texCoord) * vec4(color, opacity);
 }
--- source/ld34/ld34.d
+++ source/ld34/ld34.d
@@ -15,6 +15,10 @@ public enum WindowWidth = 1280;
 public enum WindowHeight = 720;
 
 enum GameState {
+	GameOverStart,
+	GameOver,
+	MenuStart,
+	Menu,
 	FasterAnnounceShow,
 	ControlsShow,
 	Game,
@@ -23,6 +27,10 @@ enum GameState {
 
 static double cubicin(double delta, double offset, double time) {
 	return delta * time * time * time + offset;
+}
+
+static double circularin(double delta, double offset, double time) {
+	return -delta * (sqrt(1 - time * time) - 1) + offset;
 }
 
 class LD34 : Game {
@@ -42,6 +50,12 @@ public:
 		_font.load("res/font/Roboto-Regular.ttf", 64);
 		_faster = new TTFText(_font);
 		_faster.text = "FASTER!";
+		_gameOverText = new TTFText(_font);
+		_gameOverText.text = "Game Over! Your Score: ???";
+		_gameOverDescription = new TTFText(_font);
+		_gameOverDescription.text = "Press any key to continue...";
+		_startDescription = new TTFText(_font);
+		_startDescription.text = "PRESS ANY KEY";
 
 		_sAdvance = new Sound("res/sound/advance.wav");
 		_sFaster = new Sound("res/sound/faster.wav");
@@ -66,12 +80,24 @@ public:
 		_colorTexture.registerUniform("color");
 		_colorTexture.registerUniform("transform");
 		_colorTexture.registerUniform("projection");
+		_colorTexture.registerUniform("opacity");
 		_colorTexture.set("tex", 0);
+		_colorTexture.set("opacity", 1);
 		_colorTexture.set("color", vec3(1, 1, 1));
 
 		_renderTex = new RenderTexture(WindowWidth, WindowHeight);
 		_renderQuad = RectangleShape.create(_renderTex.texture, vec2(0, 0),
 			vec2(WindowWidth, WindowHeight), vec4(0, 1, 1, 0));
+
+		_healthBar = RectangleShape.create(new Texture("res/tex/generic/health.png",
+			TextureFilterMode.Nearest, TextureFilterMode.Nearest),
+			vec2((WindowWidth - 256) * 0.5f, 0), vec2(256, 64));
+			
+		_blank = new Texture();
+		_blank.create(1, 1, cast(ubyte[]) [255, 255, 255, 255]);
+		
+		_blankShape = RectangleShape.create(_blank, vec2(0, 0), vec2(1, 1));
+		_blendShape = RectangleShape.create(_blank, vec2(0, 0), vec2(WindowWidth, WindowHeight));
 
 		randomizeKeys();
 	}
@@ -106,7 +132,8 @@ public:
 					reduceLife();
 				else
 					_sAdvance.play(0, 0);
-				_state = GameEnd;
+				if (_health > 0)
+					_state = GameEnd;
 				_time = 0;
 			}
 			break;
@@ -119,6 +146,27 @@ public:
 				_time = 0;
 				randomizeKeys();
 			}
+			break;
+		case GameOverStart:
+			if (_time > 2.0f) {
+				_state = GameOver;
+				_time = 0;
+				_gameOverText.text = "Game Over! Your Score: " ~ to!string(totalScore);
+				_blankShape.position = _gameOverText.position;
+				_blankShape.size = _gameOverText.size;
+				_blankShape.create();
+			}
+			break;
+		case GameOver:
+			break;
+		case MenuStart:
+			if (_time > 0.5f) {
+				_state = Menu;
+				_time = 0;
+				writeln("Menu");
+			}
+			break;
+		case Menu:
 			break;
 		}
 	}
@@ -145,6 +193,14 @@ public:
 				_buttonBDown = false;
 				_indicatorB.pressed = false;
 			}
+			if (_state == GameState.GameOver) {
+				_state = GameState.MenuStart;
+				_time = 0;
+			}
+			if (_state == GameState.Menu) {
+				_state = GameState.ControlsShow;
+				_time = 0;
+			}
 			break;
 		default:
 			break;
@@ -162,6 +218,10 @@ public:
 			_colorTexture.set("color", vec3(0, 0, 0));
 			window.draw(_faster, _colorTexture);
 			matrixStack.pop();
+			matrixStack.push();
+			matrixStack.top = matrixStack.top.translate2d(0, 20);
+			window.draw(_healthBar);
+			matrixStack.pop();
 			break;
 		case ControlsShow:
 			matrixStack.push();
@@ -174,9 +234,13 @@ public:
 			indicatorB.position = vec2(50, 0);
 			window.draw(indicatorB);
 			matrixStack.pop();
+			matrixStack.push();
+			matrixStack.top = matrixStack.top.translate2d(0, cubicin(84, -64,
+				1 - max(0, (_time - 2) * 2)));
+			window.draw(_healthBar);
+			matrixStack.pop();
 			break;
 		case Game:
-			window.bind();
 			window.clear(Color3.SkyBlue);
 			_currentMinigame.draw();
 			break;
@@ -190,8 +254,81 @@ public:
 				0);
 			window.draw(_renderQuad);
 			matrixStack.pop();
+			matrixStack.push();
+			matrixStack.top = matrixStack.top.translate2d(0, cubicin(84, -64, _time * 2));
+			window.draw(_healthBar);
+			matrixStack.pop();
+			break;
+		case GameOverStart:
+			_renderTex.bind();
+			_renderTex.clear(Color3.SkyBlue);
+			_currentMinigame.draw();
+			window.bind();
+			window.clear(Color3.Black);
+			matrixStack.push();
+			matrixStack.top = matrixStack.top.rotate2d(circularin(0.35f, 0, min(1, _time)));
+			matrixStack.top = matrixStack.top.translate2d(0,
+				cubicin(WindowHeight, 0, max(0, _time - 1)));
+			window.draw(_renderQuad);
+			matrixStack.pop();
+			break;
+		case GameOver:
+			window.clear(Color3.Black);
+			float offset = 0;
+			float offsetDescription = 0;
+			if(_time > 2)
+				offset = -min(1, _time - 2) * 50;
+			if(_time > 2)
+				offsetDescription = min(1, _time - 2) * 30;
+			matrixStack.push();
+			matrixStack.top = matrixStack.top.translate2d(
+				(WindowWidth - _gameOverDescription.texture.width) * 0.5f, (WindowHeight - 50) * 0.5f + offsetDescription);
+			_colorTexture.bind();
+			_colorTexture.set("color", vec3(0.5f, 0.5f, 0.5f));
+			window.draw(_gameOverDescription, _colorTexture);
+			matrixStack.pop();
+			matrixStack.push();
+			matrixStack.top = matrixStack.top.translate2d(
+				(WindowWidth - _gameOverText.texture.width) * 0.5f, (WindowHeight - 50) * 0.5f + offset);
+			_colorTexture.bind();
+			_colorTexture.set("color", vec3(Color3.Black));
+			window.draw(_blankShape, _colorTexture);
+			_colorTexture.set("color", vec3(0.8f, 0.8f, 0.8f));
+			_colorTexture.set("color", vec3(1, 1, 1));
+			window.draw(_gameOverText, _colorTexture);
+			matrixStack.pop();
+			break;
+		case MenuStart:
+			float opacity = circularin(-1, 1, _time * 2);
+			drawMenu();
+			_colorTexture.bind();
+			_colorTexture.set("color", vec3(Color3.Black));
+			_colorTexture.set("opacity", opacity);
+			window.draw(_blendShape, _colorTexture);
+			_colorTexture.set("opacity", 1.0f);
+			_colorTexture.set("color", vec3(1, 1, 1));
+			break;
+		case Menu:
+			drawMenu();
 			break;
 		}
+	}
+	
+	void drawMenu() {
+		matrixStack.push();
+		matrixStack.top = matrixStack.top.translate2d(
+			(WindowWidth - _startDescription.texture.width) * 0.5f, (WindowHeight - 50) * 0.5f);
+		_colorTexture.bind();
+		_colorTexture.set("color", vec3(0, 0, 0));
+		_colorTexture.set("opacity", (cast(int)(_time * 2) % 2) == 0 ? 0.0f : 1.0f);
+		window.draw(_startDescription, _colorTexture);
+		_colorTexture.set("color", vec3(1, 1, 1));
+		_colorTexture.set("opacity", 1.0f);
+		matrixStack.pop();
+	}
+	
+	@property int totalScore() const {
+		return cast(int) round(game * 1000 * pow(1.15, _speed));
 	}
 
 	@property float delta() const {
@@ -232,6 +369,14 @@ public:
 			return window;
 		case GameEnd:
 			return _renderTex;
+		case GameOverStart:
+			return _renderTex;
+		case GameOver:
+			return window;
+		case MenuStart:
+			return window;
+		case Menu:
+			return window;
 		}
 	}
 
@@ -241,6 +386,19 @@ public:
 
 	void reduceLife() {
 		_sHPDown.play(0, 1);
+		_health--;
+		updateHealth();
+	}
+
+	void updateHealth() {
+		if (_health <= 0)
+			_state = GameState.GameOverStart;
+		if (_health > 4)
+			_health = 4;
+
+		_healthBar.size = vec2(64 * _health, 64);
+		_healthBar.texCoords = vec4(0, 0, 0.25f * _health, 1);
+		_healthBar.create();
 	}
 
 	@property TTFFont font() {
@@ -259,18 +417,17 @@ public:
 		_speed *= 1.05f;
 		_state = GameState.FasterAnnounceShow;
 		_sFaster.play(0, 2);
-		writeln("FASTER!");
 	}
 
 	void randomizeKeys() {
 		auto key1 = uniform(0, 36);
 		auto key2 = (uniform(0, 35) + key1 + 1) % 36;
-		
+
 		_buttonADown = false;
 		_buttonBDown = false;
 		_indicatorA.pressed = false;
 		_indicatorB.pressed = false;
-		
+
 		if (key1 > 25) {
 			_buttonA = SDLK_0 + key1 - 26;
 		} else {
@@ -282,19 +439,20 @@ public:
 		} else {
 			_buttonB = SDLK_a + key2;
 		}
-		
+
 		_indicatorA.setKey([cast(char) _buttonA]);
 		_indicatorB.setKey([cast(char) _buttonB]);
 	}
 
 private:
 	float _delta;
+	int _health = 4;
 	int _buttonA;
 	int _buttonB;
 	bool _buttonADown;
 	bool _buttonBDown;
 	TTFFont _font;
-	TTFText _faster;
+	TTFText _faster, _gameOverText, _gameOverDescription, _startDescription;
 	KeyIndicator _indicatorA;
 	KeyIndicator _indicatorB;
 	Minigame[] _minigames;
@@ -305,10 +463,13 @@ private:
 	StopWatch _gameTimer;
 	RenderTexture _renderTex;
 	RectangleShape _renderQuad;
+	RectangleShape _healthBar;
 	float _time = 0;
 	float _speed = 1;
-	GameState _state = GameState.ControlsShow;
+	GameState _state = GameState.MenuStart;
 	Sound _sAdvance, _sFaster, _sHPDown, _sHPUp;
+	Texture _blank;
+	RectangleShape _blankShape, _blendShape;
 
 	void newGame() {
 		_currentMinigameIdx++;
