b7e7f3857361beeabd868f68c2161d3e2b8a152a
WebFreak001 <janju007@web.de>
2015-12-12 17:54:25 +0100

    Added button game

--- source/ld34/render/keyindicator.d
+++ source/ld34/render/keyindicator.d
@@ -22,7 +22,7 @@ public:
 		matrixStack.top = matrixStack.top * transform;
 		background.texture = _pressed ? down : up;
 		background.draw(target, shader);
-		matrixStack.top = matrixStack.top * mat4.translation(24, 20 + (_pressed ? 4 : 0), 0);
+		matrixStack.top = matrixStack.top * mat4.translation(24, 20 + (_pressed ? 6 : 0), 0);
 		text.draw(target, shader);
 		matrixStack.pop();
 	}
