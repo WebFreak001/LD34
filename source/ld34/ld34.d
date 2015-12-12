20e88511fd3ddd9fe5a8ec086c84352db96f6362
WebFreak001 <janju007@web.de>
2015-12-12 07:20:36 +0100

    Added support for drawing stuff

--- source/ld34/ld34.d
+++ source/ld34/ld34.d
@@ -20,7 +20,43 @@ public:
 
 	override void load() {
 		registerMinigame();
-		currentMinigame = _minigames[0];
+		currentMinigame = _minigames[9];
+
+		_colorTexture = new ShaderProgram();
+		Shader vertex = new Shader();
+		vertex.load(ShaderType.Vertex, "#version 330
+layout(location = 0) in vec3 in_position;
+layout(location = 1) in vec2 in_tex;
+uniform mat4 transform;
+uniform mat4 projection;
+out vec2 texCoord;
+void main()
+{
+	gl_Position = projection * transform * vec4(in_position, 1);
+	texCoord = in_tex;
+}
+");
+		Shader fragment = new Shader();
+		fragment.load(ShaderType.Fragment, "#version 330
+uniform sampler2D tex;
+uniform vec3 color;
+in vec2 texCoord;
+layout(location = 0) out vec4 out_frag_color;
+void main()
+{
+	out_frag_color = texture(tex, texCoord) * vec4(color, 1);
+}
+");
+		_colorTexture.attach(vertex);
+		_colorTexture.attach(fragment);
+		_colorTexture.link();
+		_colorTexture.bind();
+		_colorTexture.registerUniform("tex");
+		_colorTexture.registerUniform("color");
+		_colorTexture.registerUniform("transform");
+		_colorTexture.registerUniform("projection");
+		_colorTexture.set("tex", 0);
+		_colorTexture.set("color", vec3(1, 1, 1));
 	}
 
 	override void update(float delta) {
@@ -29,7 +65,7 @@ public:
 
 		if (_currentMinigame.isDone) {
 			writeln("isWon: ", _currentMinigame.hasWon);
-			currentMinigame = _minigames[0];
+			currentMinigame = _minigames[9];
 		}
 	}
 
@@ -90,6 +126,14 @@ public:
 		return _currentMinigame;
 	}
 
+	@property Window target() {
+		return this.window;
+	}
+
+	@property auto colorTextureShader() {
+		return _colorTexture;
+	}
+
 private:
 	float _delta;
 	int _buttonA;
@@ -99,6 +143,7 @@ private:
 	Minigame[] _minigames;
 	Minigame _currentMinigame;
 	int game = 0;
+	ShaderProgram _colorTexture;
 
 	void registerMinigame() {
 		import ld34.minigame.testgame : TestGame;
