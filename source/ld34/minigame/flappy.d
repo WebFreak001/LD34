c3557f7ef762470a901cca2f0aea4f1be8ffe40e
WebFreak001 <janju007@web.de>
2015-12-13 01:48:39 +0100

    Removed delta time from flappy

--- source/ld34/minigame/flappy.d
+++ source/ld34/minigame/flappy.d
@@ -17,33 +17,33 @@ public:
 			TextureFilterMode.Nearest, TextureFilterMode.Nearest);
 		//dfmt off
 		pipes = [
-			RectangleShape.create(pipe, vec2(1600, -10), vec2(64, 256), vec4(0, 1, 1, 0)),
-			RectangleShape.create(pipe, vec2(2000, -64), vec2(64, 256), vec4(0, 1, 1, 0)),
-			RectangleShape.create(pipe, vec2(2400, -100), vec2(64, 256), vec4(0, 1, 1, 0)),
-			RectangleShape.create(pipe, vec2(2800, -150), vec2(64, 256), vec4(0, 1, 1, 0)),
-			RectangleShape.create(pipe, vec2(3200, -30), vec2(64, 256), vec4(0, 1, 1, 0)),
-			RectangleShape.create(pipe, vec2(3600, -64), vec2(64, 256), vec4(0, 1, 1, 0)),
-			RectangleShape.create(pipe, vec2(4000, -100), vec2(64, 256), vec4(0, 1, 1, 0)),
-			RectangleShape.create(pipe, vec2(4400, -150), vec2(64, 256), vec4(0, 1, 1, 0)),
-			RectangleShape.create(pipe, vec2(4800, -30), vec2(64, 256), vec4(0, 1, 1, 0)),
-			RectangleShape.create(pipe, vec2(5200, -64), vec2(64, 256), vec4(0, 1, 1, 0)),
-			RectangleShape.create(pipe, vec2(5600, -100), vec2(64, 256), vec4(0, 1, 1, 0)),
-			RectangleShape.create(pipe, vec2(6000, -150), vec2(64, 256), vec4(0, 1, 1, 0)),
-			RectangleShape.create(pipe, vec2(6400, -30), vec2(64, 256), vec4(0, 1, 1, 0)),
+			RectangleShape.create(pipe, vec2(600, -10), vec2(64, 256), vec4(0, 1, 1, 0)),
+			RectangleShape.create(pipe, vec2(1000, -64), vec2(64, 256), vec4(0, 1, 1, 0)),
+			RectangleShape.create(pipe, vec2(1400, -100), vec2(64, 256), vec4(0, 1, 1, 0)),
+			RectangleShape.create(pipe, vec2(1800, -150), vec2(64, 256), vec4(0, 1, 1, 0)),
+			RectangleShape.create(pipe, vec2(2200, -30), vec2(64, 256), vec4(0, 1, 1, 0)),
+			RectangleShape.create(pipe, vec2(2600, -64), vec2(64, 256), vec4(0, 1, 1, 0)),
+			RectangleShape.create(pipe, vec2(3000, -100), vec2(64, 256), vec4(0, 1, 1, 0)),
+			RectangleShape.create(pipe, vec2(3400, -150), vec2(64, 256), vec4(0, 1, 1, 0)),
+			RectangleShape.create(pipe, vec2(3800, -30), vec2(64, 256), vec4(0, 1, 1, 0)),
+			RectangleShape.create(pipe, vec2(4200, -64), vec2(64, 256), vec4(0, 1, 1, 0)),
+			RectangleShape.create(pipe, vec2(4600, -100), vec2(64, 256), vec4(0, 1, 1, 0)),
+			RectangleShape.create(pipe, vec2(5000, -150), vec2(64, 256), vec4(0, 1, 1, 0)),
+			RectangleShape.create(pipe, vec2(5400, -30), vec2(64, 256), vec4(0, 1, 1, 0)),
 			
-			RectangleShape.create(pipe, vec2(1600, WindowHeight - 110), vec2(64, 256), vec4(0, 0, 1, 1)),
-			RectangleShape.create(pipe, vec2(2000, WindowHeight - 164), vec2(64, 256), vec4(0, 0, 1, 1)),
-			RectangleShape.create(pipe, vec2(2400, WindowHeight - 200), vec2(64, 256), vec4(0, 0, 1, 1)),
-			RectangleShape.create(pipe, vec2(2800, WindowHeight - 250), vec2(64, 256), vec4(0, 0, 1, 1)),
-			RectangleShape.create(pipe, vec2(3200, WindowHeight - 130), vec2(64, 256), vec4(0, 0, 1, 1)),
-			RectangleShape.create(pipe, vec2(3600, WindowHeight - 164), vec2(64, 256), vec4(0, 0, 1, 1)),
-			RectangleShape.create(pipe, vec2(4000, WindowHeight - 200), vec2(64, 256), vec4(0, 0, 1, 1)),
-			RectangleShape.create(pipe, vec2(4400, WindowHeight - 250), vec2(64, 256), vec4(0, 0, 1, 1)),
-			RectangleShape.create(pipe, vec2(4800, WindowHeight - 130), vec2(64, 256), vec4(0, 0, 1, 1)),
-			RectangleShape.create(pipe, vec2(5200, WindowHeight - 164), vec2(64, 256), vec4(0, 0, 1, 1)),
-			RectangleShape.create(pipe, vec2(5600, WindowHeight - 200), vec2(64, 256), vec4(0, 0, 1, 1)),
-			RectangleShape.create(pipe, vec2(6000, WindowHeight - 250), vec2(64, 256), vec4(0, 0, 1, 1)),
-			RectangleShape.create(pipe, vec2(6400, WindowHeight - 130), vec2(64, 256), vec4(0, 0, 1, 1)),
+			RectangleShape.create(pipe, vec2(600, WindowHeight - 110), vec2(64, 256), vec4(0, 0, 1, 1)),
+			RectangleShape.create(pipe, vec2(1000, WindowHeight - 164), vec2(64, 256), vec4(0, 0, 1, 1)),
+			RectangleShape.create(pipe, vec2(1400, WindowHeight - 200), vec2(64, 256), vec4(0, 0, 1, 1)),
+			RectangleShape.create(pipe, vec2(1800, WindowHeight - 250), vec2(64, 256), vec4(0, 0, 1, 1)),
+			RectangleShape.create(pipe, vec2(2200, WindowHeight - 130), vec2(64, 256), vec4(0, 0, 1, 1)),
+			RectangleShape.create(pipe, vec2(2600, WindowHeight - 164), vec2(64, 256), vec4(0, 0, 1, 1)),
+			RectangleShape.create(pipe, vec2(3000, WindowHeight - 200), vec2(64, 256), vec4(0, 0, 1, 1)),
+			RectangleShape.create(pipe, vec2(3400, WindowHeight - 250), vec2(64, 256), vec4(0, 0, 1, 1)),
+			RectangleShape.create(pipe, vec2(3800, WindowHeight - 130), vec2(64, 256), vec4(0, 0, 1, 1)),
+			RectangleShape.create(pipe, vec2(4200, WindowHeight - 164), vec2(64, 256), vec4(0, 0, 1, 1)),
+			RectangleShape.create(pipe, vec2(4600, WindowHeight - 200), vec2(64, 256), vec4(0, 0, 1, 1)),
+			RectangleShape.create(pipe, vec2(5000, WindowHeight - 250), vec2(64, 256), vec4(0, 0, 1, 1)),
+			RectangleShape.create(pipe, vec2(5400, WindowHeight - 130), vec2(64, 256), vec4(0, 0, 1, 1)),
 		];
 		//dfmt on
 	}
@@ -52,8 +52,8 @@ public:
 		super.start(difficulty);
 		x = 200;
 		y = 200;
-		xv = 1;
-		yv = 0.2f;
+		xv = 0.01f;
+		yv = 0.002f;
 		_won = true;
 	}
 
@@ -62,11 +62,11 @@ public:
 
 	override void update() {
 		if (_game.isButtonADown)
-			xv += _game.delta * 0.1f;
+			xv += 0.001f;
 		if (_game.isButtonBDown)
-			yv += _game.delta * 2;
+			yv += 0.002f;
 		y -= yv;
-		yv -= _game.delta;
+		yv -= 0.001f;
 		x += xv;
 
 		foreach (pipe; pipes) {
